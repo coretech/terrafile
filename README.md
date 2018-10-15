b9f3781a95392f6a5fe8212a2ce0d93d6cf5220f
nritholtz <nritholtz@gmail.com>
2018-10-15 16:03:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Terrafile
+
+Terrafile is a binary written in Go to systematically manage external modules from Github for use in Terraform. See this [article](http://bensnape.com/2016/01/14/terraform-design-patterns-the-terrafile/) for more information on how it was introduced in a Ruby rake task.
+
+This is currently an experimental WIP.
---
+++ cmd/terrafile/terrafile.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	"github.com/jessevdk/go-flags"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+type module struct {
+	Source  string `yaml:"source"`
+	Version string `yaml:"version"`
+}
+
+var opts struct {
+	ModulePath string `short:"p" long:"module_path" default:"./vendor/modules" description:"File path to install generated terraform modules"`
+
+	TerrrafilePath string `short:"f" long:"terrafile_file" default:"." description:"File path in which the Terrafile file is located"`
+}
+
+// ModulePath File path in which the modules will be exported
+const ModulePath = "vendor/modules"
+
+func gitClone(repository string, version string, moduleName string) {
+	log.Printf("[*] Checking out %s of %s \n", version, repository)
+	cmd := exec.Command("git", "clone", "-b", version, repository, moduleName)
+	cmd.Dir = ModulePath
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	_, err := flags.Parse(&opts)
+
+	// Invalid choice
+	if err != nil {
+		os.Exit(1)
+	}
+
+	// Read File
+	yamlFile, err := ioutil.ReadFile(fmt.Sprint(opts.TerrrafilePath, "/Terrafile"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Parse File
+	var config map[string]module
+	if err := yaml.Unmarshal(yamlFile, &config); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Clone modules
+	os.RemoveAll(opts.ModulePath)
+	os.MkdirAll(opts.ModulePath, os.ModePerm)
+	for key, module := range config {
+		gitClone(module.Source, module.Version, key)
+	}
+}
